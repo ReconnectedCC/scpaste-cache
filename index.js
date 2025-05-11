d12cf8b00bab0e28c881a71eb7e4732375779cdf
EmmaKnijn <64010592+EmmaKnijn@users.noreply.github.com>
2025-05-11 08:22:27 +0200

    add "proxy" functionality, fix a bug

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ app.get('/paste/:id', (req, res) => {
 
 app.post('/paste/:id', (req, res) => {
 
-    https.get('https://p.sc3.io/api/v1/pastes/' + req.params.id + "/raw", res => {
+    https.get('https://p.sc3.io/api/v1/pastes/' + req.params.id + "/raw", httpres => {
         let data = [];
-        const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-        console.log('Status Code:', res.statusCode);
+        const headerDate = httpres.headers && httpres.headers.date ? httpres.headers.date : 'no response date';
+        console.log('Status Code:', httpres.statusCode);
         console.log('Date in Response header:', headerDate);
 
-        res.on('data', chunk => {
+        httpres.on('data', chunk => {
             data.push(chunk);
         });
 
-        res.on('end', () => {
+        httpres.on('end', () => {
             console.log('Response ended: ');
             const content = Buffer.concat(data).toString();
             if (!fs.existsSync('./pastes')) {
@@ -50,7 +50,52 @@ app.post('/paste/:id', (req, res) => {
     }).on('error', err => {
         console.log('Error: ', err.message);
     });
+})
 
+app.get("/api/v1/pastes/:id/raw", (req, res) => {
+    https.get('https://p.sc3.io/api/v1/pastes/' + req.params.id + "/raw", httpres => {
+        let data = [];
+        const headerDate = httpres.headers && httpres.headers.date ? httpres.headers.date : 'no response date';
+        console.log('Status Code:', httpres.statusCode);
+        console.log('Date in Response header:', headerDate);
+
+        httpres.on('data', chunk => {
+            data.push(chunk);
+        });
+
+        httpres.on('end', () => {
+            console.log('Response ended: ');
+            const content = Buffer.concat(data).toString();
+            if (!fs.existsSync('./pastes')) {
+                fs.mkdir(
+                    './pastes',
+                    err => {
+                        if (err) {
+                            console.error(err);
+                        }
+                    })
+            }
+            if (httpres.statusCode == 404) {
+                res.statusCode = 404;
+                res.send('Not found in scpaste')
+                return;
+            } else
+                res.statusCode = 200;
+                res.send(content);
+                fs.writeFileSync('./pastes/' + req.params.id, content);
+            });
+    }).on('error', err => {
+        fs.readFile("./pastes/" + req.params.id, (err, data) => {
+            if (!err && data) {
+                res.statusCode = 200;
+                res.send(data)
+            } else {
+                res.statusCode = 404;
+                res.send('Not found in cache and scpaste is offline')
+
+            }
+        });
+    });
 })
 
 app.listen(port, () => {
